37e6fe756583932c540614be199a8ccf943610c2
Neal R <nealjrobison@gmail.com>
2020-03-19 16:58:00 -0400

    modified the server.sh script to use development mode instead of production.

--- server.sh
+++ server.sh
@@ -1,3 +1,9 @@
-echo "Serving njr-website-react:"
-serve -s build
+#Use once "serve" behavior is fixed
+#echo "Serving njr-website-react:"
+#serve -s build
+
+# Used until I figure out how to use the build/...
+echo "Starting React App in Development Mode..."
+npm start
+
 
