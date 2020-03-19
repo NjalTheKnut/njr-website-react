d6e7f67bc936701239719061bbeed471d9c58f67
Neal R <nealjrobison@gmail.com>
2020-03-19 19:04:34 -0400

    Removed lingering references to thumbnails on image carousel, as well as the indicators.

--- src/Home.js
+++ src/Home.js
@@ -59,9 +59,9 @@ class Home extends React.Component {
                     imagesWidth="800px"
                     imagesHeight="450px"
                     imagesHeightMobile="56vw"
-                    thumbnailsWidth="920px"
-                    thumbnailsHeight="12vw"
-                    indicators 
+                    //thumbnailsWidth="920px"
+                    //thumbnailsHeight="12vw"
+                    //indicators 
                     //thumbnails 
                     fixedImagesHeight
                     infinite
