9dc4d1bcca4377476fca2d094b82bc05f4ec3d54
Neal R <nealjrobison@gmail.com>
2020-03-18 21:03:18 -0400

    Added files from original static page

---
+++ public/resources/NJR.js
@@ -0,0 +1,35 @@
+window.onload = function(){
+	var images = [
+		'https://njaltheknut.github.io/NJR/Web/CompTIA_Security+_ce_certificate.jpg',
+		'https://njaltheknut.github.io/NJR/Web/cert-1014-7239257.jpg',
+		'https://njaltheknut.github.io/NJR/Web/cert-1023-7239257.jpg',
+		'https://njaltheknut.github.io/NJR/Web/cert-1024-7239257.jpg',
+		'https://njaltheknut.github.io/NJR/Web/cert-1051-7239257.jpg',
+		'https://njaltheknut.github.io/NJR/Web/cert-1059-7239257.jpg',
+		'https://njaltheknut.github.io/NJR/Web/cert-1060-7239257.jpg',
+		'https://njaltheknut.github.io/NJR/Web/cert-1068-7239257.jpg',
+		'https://njaltheknut.github.io/NJR/Web/cert-1073-7239257.jpg',
+		'https://njaltheknut.github.io/NJR/Web/cert-1080-7239257.jpg',
+		'https://njaltheknut.github.io/NJR/Web/cert-1081-7239257.jpg',
+	];
+	var num = 0;
+	var prevButton = document.getElementById('prevButton');
+	prevButton.onclick = function(){
+		var slider = document.getElementById('slider');
+		num--;
+		if(num < 0){
+			num = images.length-1;
+		}
+		slider.src = images[num];
+		
+	}
+	var nextButton = document.getElementById('nextButton');
+	nextButton.onclick = function(){
+		var slider = document.getElementById('slider');
+		num++;
+		if(num >= images.length){
+			num = 0;
+		}
+		slider.src = images[num];
+	}
+}
