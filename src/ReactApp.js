a116d52e9ca6951bbbf6502daf35bcbc7ecec00e
Neal R <nealjrobison@gmail.com>
2020-03-18 21:06:14 -0400

    Feature Added: Change title of React App page

--- src/ReactApp.js
+++ src/ReactApp.js
@@ -2,29 +2,38 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-function ReactApp() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Neal was Here!
-            <br />Merged my old static HTML/CSS/JavaScript page with this Boilerplate React App!
-            <br />
+class ReactApp extends React.Component {
+    setPageTitle() {
+        var headElement = (document).querySelector("head");
+        var PageTitle = headElement.querySelector("title");
+        PageTitle.innerHTML = "Neal Robison | React App";
+    }
 
-            {/* Edit <code>src/App.js</code> and save to reload. */}
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+    render() {
+        this.setPageTitle();
+        return (
+        <div className="App">
+            <header className="App-header">
+            <img src={logo} className="App-logo" alt="logo" />
+            <p>
+                Neal was Here!
+                <br />Merged my old static HTML/CSS/JavaScript page with this Boilerplate React App!
+                <br />
+
+                {/* Edit <code>src/App.js</code> and save to reload. */}
+            </p>
+            <a
+                className="App-link"
+                href="https://reactjs.org"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+                Learn React
+            </a>
+            </header>
+        </div>
+        );
+    };
   }
   
   export default ReactApp;
