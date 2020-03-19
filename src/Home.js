05067c1a930ddaabb2718b28874f612ad46373ed
Neal R <nealjrobison@gmail.com>
2020-03-19 18:59:45 -0400

    Removed thumbnails from image carousel

--- src/Home.js
+++ src/Home.js
@@ -62,7 +62,7 @@ class Home extends React.Component {
                     thumbnailsWidth="920px"
                     thumbnailsHeight="12vw"
                     indicators 
-                    thumbnails 
+                    //thumbnails 
                     fixedImagesHeight
                     infinite
                 />
